5b71271b3637d375de827e19353d2214fbf44f58
anicrob <arobbins@adaptavist.com>
2023-11-17 12:43:56 -0500

    removed search url

--- helpers.js
+++ helpers.js
@@ -9,7 +9,7 @@ const searchFilters = async () => {
     0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750,
     800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400,
     1450, 1500, 1550, 1600, 1650, 1700, 1750, 1800, 1850, 1900, 1950, 2000,
-    2050, 2100, 2150, 2200, 2250, 2300, 2350, 2400, 2450, 2500
+    2050, 2100, 2150, 2200, 2250, 2300, 2350, 2400, 2450, 2500,
   ];
   await Promise.all(
     index.map(async (i) => {
@@ -27,12 +27,20 @@ const searchFilters = async () => {
         let { values } = await response.json();
         if (values.length > 0) {
           values.map((filter) => {
-            if (!!filter.jql.toLowerCase().includes(`${process.env.SEARCH_KEYWORD}`.toLowerCase())) {
+            if (
+              !!filter.jql
+                .toLowerCase()
+                .includes(`${process.env.SEARCH_KEYWORD}`.toLowerCase())
+            ) {
               filterDataMatch.push(filter);
             }
           });
         } else if (values.length === 1) {
-          if (!!values.jql.toLowerCase().includes(`${process.env.SEARCH_KEYWORD}`.toLowerCase())) {
+          if (
+            !!values.jql
+              .toLowerCase()
+              .includes(`${process.env.SEARCH_KEYWORD}`.toLowerCase())
+          ) {
             filterDataMatch.push(...values);
           }
           return;
@@ -55,7 +63,6 @@ const refineDetails = async (allFilterDetails) => {
       description: description ? description : " ",
       owner: owner.displayName,
       viewUrl,
-      searchUrl,
       jql,
     })
   );
